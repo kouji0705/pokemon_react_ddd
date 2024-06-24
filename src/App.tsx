f82f9ec20baaf4d43639658fed914ad8b7e5806e
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-06-24 21:37:44 +0900

    add:

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import { useState } from "react";
 import Battle from "@components/Battle";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	// return <div>aaa</div>;
 	return <Battle />;
 }
 
